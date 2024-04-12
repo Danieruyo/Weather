4dcbf9319206ae26179b7992e35beea7ff30484c
Daniel de Abreu <danielabreuintz@gmail.com>
2024-04-12 19:22:13 -0300

    Update script.js

--- WeatherJs/script.js
+++ WeatherJs/script.js
@@ -12,7 +12,7 @@ search.addEventListener('click', () => {
     if (city == '')
     return;
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json => { 
+    fetch( { 
 
         if (json.cod == '404') {
             cityHide.textContent = city;
